6065a66d63d235c53e3bd8a906c2ca61fbff42b7
AnhLT <tuananh220695@gmail.com>
2021-08-16 23:39:36 +0700

    update

--- scripts/docker-prune.sh
+++ scripts/docker-prune.sh
@@ -4,4 +4,4 @@
 docker rm -f $(docker ps -a | grep Exited | awk '{print $1}')
 
 # docker prune
-docker image prune -f & docker container prune -f & docker volume prune -f
+docker image prune -f && docker container prune -f && docker volume prune -f
